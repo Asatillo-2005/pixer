c9be4ddc787a4bb7803c5c9a345ae6f821847323
Asatillo-2005 <gsatoro517@gmail.com>
2023-08-09 15:30:57 +0500

    push new

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,39 +5,41 @@ import instagram from "../../assets/svg/instagram.svg"
 
 function Footer() {
     return (
-        <>
-            <div className="container">
-                <div className="footer-box">
-                    <ul className="footer-item">
-                        <li className="footer-items">
-                            <h4 className="footer-titles">Copyright 2020</h4>
-                            <p className="footer-text">Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget sollicitudin massa pellentesque in. Vivamus mattis eros at sem pulvinar tincidunt.</p>
-                        </li>
-                        <li className="footer-items">
-                            <h4 className="footer-titles">Sayt sahifalari</h4>
-                            <div className="footer-content">
-                            <a className="footer-texts" href="#">Bosh sahifa</a>
-                            <a className="footer-text" href="#">Xizmatlar</a>
-                            <a className="footer-text" href="#">Portfolio</a>
-                            <a className="footer-text" href="#">Jamoa</a>
-                            <a className="footer-text" href="#">Blog</a>
-                            <a className="footer-text" href="#">Kontaktlar</a>
-                            </div>
-                          
-                        </li>
-                        <li className="footer-items">
-                            <h4 className="footer-titles">Biz internetda</h4>
-                            <div className="footer-content">
-                            <a className="icons-name" href="#">
-                            <img className="footer-img" src={telegram} alt="erroe"/>telegram</a>
-                            <a className="icons-name" href="#">  <img className="footer-img" src={facebook} alt="erroe"/>facebook</a>       
-                            <a className="icons-name" href="#"><img className="footer-img" src={instagram} alt="erroe"/>instagram</a>
-                            </div>
-                        </li>
-                    </ul>
+            <section className="section-footer">
+                <div className="container">
+                    <div className="footer-box">
+                        <ul className="footer-item">
+                            <li className="footer-items">
+                                <h4 className="footer-titles">Copyright 2020</h4>
+                                <p className="footer-text">Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget sollicitudin massa pellentesque in. Vivamus mattis eros at sem pulvinar tincidunt.</p>
+                            </li>
+                            <li className="footer-items">
+                                <h4 className="footer-titles">Sayt sahifalari</h4>
+                                <div className="footer-content">
+                                    <a className="footer-texts" href="#">Bosh sahifa</a>
+                                    <a className="footer-text" href="#">Xizmatlar</a>
+                                    <a className="footer-text" href="#">Portfolio</a>
+                                    <a className="footer-text" href="#">Jamoa</a>
+                                    <a className="footer-text" href="#">Blog</a>
+                                    <a className="footer-text" href="#">Kontaktlar</a>
+                                </div>
+
+                            </li>
+                            <li className="footer-items">
+                                <h4 className="footer-titles">Biz internetda</h4>
+                                <div className="footer-content">
+                                    <a className="icons-name" href="#">
+                                        <img className="footer-img" src={telegram} alt="erroe" />telegram</a>
+                                    <a className="icons-name" href="#">  <img className="footer-img" src={facebook} alt="erroe" />facebook</a>
+                                    <a className="icons-name" href="#"><img className="footer-img" src={instagram} alt="erroe" />instagram</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </>
+            </section>
+
+
     )
 }
 
--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -2,21 +2,21 @@ import "./menu.css"
 
 function Menu() {
     return (
-        <>
-            <div className="container">
+            <section className="section-menu">
+                <div className="container">
 
-                <div className="menu-box">
-                    <h2 className="menu-title">Buyurtma berish</h2>
-                    <p className="menu-text">Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget sollicitudin massa pellentesque in. Vivamus mattis eros at sem pulvinar</p>
-                    <form className="menu-form">
-                        <input className="input1" type="text" placeholder="Ismingiz" />
-                        <input className="input1" type="number" placeholder="Telefon raqamingiz" />
-                        <input className="input1" type="data" placeholder="Xizmat turi" />
-                        <button className="menu-btn">Buyurtma berish</button>
-                    </form>
+                    <div className="menu-box">
+                        <h2 className="menu-title">Buyurtma berish</h2>
+                        <p className="menu-text">Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget sollicitudin massa pellentesque in. Vivamus mattis eros at sem pulvinar</p>
+                        <form className="menu-form">
+                            <input className="input1" type="text" placeholder="Ismingiz" />
+                            <input className="input1" type="number" placeholder="Telefon raqamingiz" />
+                            <input className="input1" type="data" placeholder="Xizmat turi" />
+                            <button className="menu-btn">Buyurtma berish</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </>
+            </section>
     )
 }
 
